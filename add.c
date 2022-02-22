e0ca283b4bf1a792ce9438c737c4648f90207ee4
PM22021 <94799112+PM22021@users.noreply.github.com>
2022-02-22 20:11:26 +0530

    modified add program

--- add.c
+++ add.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main() {   
-    int number;
-   
+    int number1;
+    int number2
+   //enter the integer
     printf("Enter an integer: ");  
     
     // reads and stores input
-    scanf("%d", &number);
-
+    scanf("%d", &number1);
+    //enter the integer
+    printf("Enter an integer: ");  
+        // reads and stores input
+    scanf("%d", &number2);
     // displays output
-    printf("You entered: %d", number);
+    printf("You entered: %d %d", number1,number2);
     
     return 0;
 }
